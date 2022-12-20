26df52eefec0bae1ec8247d3f70cc3bc108f5f8c
Lens <LensPlaysGames@gmail.com>
2022-12-20 10:16:36 -0800

    [4.1/Emacs Lisp] Helpers

--- 4/solution.el
+++ 4/solution.el
@@ -53,6 +53,29 @@
 
     out))
 
+(defun bounds-l-contains-r-p (bounds)
+  "Return T iff the lower bound of lhs of BOUNDS is less than the lower bound of rhs,
+AND the upper bound of lhs is greater than the upper bound of rhs."
+  (let ((lhs (car bounds))
+        (rhs (cdr bounds)))
+    (when (< (car lhs) (car rhs))
+      (> (cdr lhs) (cdr rhs)))))
+
+(defun bounds-r-contains-l-p (bounds)
+  "Return T iff the lower bound of rhs of BOUNDS is less than the lower bound of lhs,
+AND the upper bound of rhs is greater than the upper bound of lhs."
+  (let ((lhs (car bounds))
+        (rhs (cdr bounds)))
+    (when (< (car rhs) (car lhs))
+      (> (cdr rhs) (cdr lhs)))))
+
+
+(defun bounds-contains-p (bounds)
+  "Return T iff one pair of BOUNDS is entirely contained by the other.
+Otherwise NIL."
+  (or (bounds-l-contains-r-p bounds)
+      (bounds-r-contains-l-p bounds)))
+
 (with-temp-buffer
   (insert-file-contents "input.txt")
   (print (decode-bounds))
